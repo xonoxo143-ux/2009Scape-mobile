b62970f8ee66bd4288cd2940be3322246019f163
xonoxo143-ux <xonoxo143@gmail.com>
2026-08-19 02:49:00 -0400

    Launch RT4 client only after local Grand League server is ready

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/ScapeLauncher.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/ScapeLauncher.java
@@ -1,14 +1,15 @@
 package net.kdt.pojavlaunch;
 
 import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.kdt.mcgui.ProgressLayout;
 
+import net.kdt.pojavlaunch.grandleague.GrandLeagueServerController;
 import net.kdt.pojavlaunch.progresskeeper.ProgressKeeper;
 import net.kdt.pojavlaunch.services.ProgressServiceKeeper;
 
@@ -17,8 +18,6 @@ public class ScapeLauncher extends BaseActivity {
     private TextView settings;
     private Button playHD;
     private Button playSD;
-    private static final int FILE_SELECT_CODE_JSON = 0;
-    private static final int FILE_SELECT_CODE_ZIP = 1;
     private ProgressServiceKeeper mProgressServiceKeeper;
     private ProgressLayout mProgressLayout;
 
@@ -26,6 +25,13 @@ public class ScapeLauncher extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dummy_launcher);
+
+        try {
+            Tools.copyAssetFile(this, "config.json", Tools.DIR_DATA, true);
+        } catch (Exception exception) {
+            Log.e("GrandLeagueClient", "Unable to enforce localhost client config", exception);
+        }
+
         settings = findViewById(R.id.settings);
         playHD = findViewById(R.id.playHD);
         playSD = findViewById(R.id.playSD);
@@ -33,27 +39,25 @@ public class ScapeLauncher extends BaseActivity {
 
         ProgressKeeper.addTaskCountListener((mProgressServiceKeeper = new ProgressServiceKeeper(this)));
         ProgressKeeper.addTaskCountListener(mProgressLayout);
-
         mProgressLayout.observe(ProgressLayout.UNPACK_RUNTIME);
         mProgressLayout.observe(ProgressLayout.INSTALL_MODPACK);
 
+        GrandLeagueServerController.ensureStarted(this);
+
         playHD.setOnClickListener(view -> {
-            if(!runtimeReady()) return;
-            Intent intent = new Intent(ScapeLauncher.this, MainActivity.class);
-            startActivity(intent);
+            if (!runtimeReady()) return;
+            GrandLeagueServerController.launchWhenReady(ScapeLauncher.this, MainActivity.class);
         });
 
         playSD.setOnClickListener(view -> {
-            if(!runtimeReady()) return;
-            Intent intent = new Intent(ScapeLauncher.this, JavaGUILauncherActivity.class);
-            startActivity(intent);
+            if (!runtimeReady()) return;
+            GrandLeagueServerController.launchWhenReady(ScapeLauncher.this, JavaGUILauncherActivity.class);
         });
         settings.setOnClickListener(view -> showBottomDialog());
     }
 
-
-    private boolean runtimeReady(){
-        if(mProgressLayout.hasProcesses()){
+    private boolean runtimeReady() {
+        if (mProgressLayout.hasProcesses()) {
             Toast.makeText(this, R.string.tasks_ongoing, Toast.LENGTH_LONG).show();
             return false;
         }
