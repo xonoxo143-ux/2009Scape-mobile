5d5379853eacc93f51caa5977d07940cb537d280
xonoxo143-ux <xonoxo143@gmail.com>
2026-08-19 02:00:01 -0400

    Add headless JVM launcher for embedded Grand League server

---
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/grandleague/HeadlessJavaLauncher.java
@@ -0,0 +1,81 @@
+package net.kdt.pojavlaunch.grandleague;
+
+import android.content.Context;
+import android.os.Build;
+import android.system.ErrnoException;
+import android.system.Os;
+import android.util.Log;
+
+import com.oracle.dalvik.VMLauncher;
+
+import net.kdt.pojavlaunch.Tools;
+import net.kdt.pojavlaunch.multirt.MultiRTUtils;
+import net.kdt.pojavlaunch.multirt.Runtime;
+import net.kdt.pojavlaunch.utils.JREUtils;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TimeZone;
+
+/** Launches a non-graphical Java program inside the current Android process. */
+final class HeadlessJavaLauncher {
+    private static final String TAG = "GrandLeagueJVM";
+
+    private HeadlessJavaLauncher() { }
+
+    static int launch(Context context, String runtimeName, File workingDirectory, List<String> programArgs) throws Throwable {
+        Tools.initContextConstants(context);
+        Runtime runtime = MultiRTUtils.forceReread(runtimeName);
+        if (runtime.versionString == null) {
+            throw new IllegalStateException("Grand League runtime is not installed: " + runtimeName);
+        }
+
+        String runtimeHome = MultiRTUtils.getRuntimeHome(runtimeName).getAbsolutePath();
+        JREUtils.relocateLibPath(runtime, runtimeHome);
+        setEnvironment(runtimeHome, workingDirectory);
+
+        List<String> args = new ArrayList<>();
+        args.add("java");
+        args.add("-Djava.home=" + runtimeHome);
+        args.add("-Djava.io.tmpdir=" + context.getCacheDir().getAbsolutePath());
+        args.add("-Duser.home=" + workingDirectory.getAbsolutePath());
+        args.add("-Duser.language=" + System.getProperty("user.language", "en"));
+        args.add("-Duser.timezone=" + TimeZone.getDefault().getID());
+        args.add("-Dos.name=Linux");
+        args.add("-Dos.version=Android-" + Build.VERSION.RELEASE);
+        args.add("-Djava.awt.headless=true");
+        args.add("-Xms128M");
+        args.add("-Xmx1024M");
+        args.addAll(programArgs);
+
+        JREUtils.initJavaRuntime(runtimeHome);
+        JREUtils.setupExitTrap(context.getApplicationContext());
+        if (JREUtils.chdir(workingDirectory.getAbsolutePath()) != 0) {
+            throw new IllegalStateException("Unable to enter Grand League server directory");
+        }
+
+        Log.i(TAG, "Launching local Grand League server with Java " + runtime.versionString);
+        return VMLauncher.launchJVM(args.toArray(new String[0]));
+    }
+
+    private static void setEnvironment(String runtimeHome, File workingDirectory) throws ErrnoException {
+        Map<String, String> env = new LinkedHashMap<>();
+        env.put("POJAV_NATIVEDIR", Tools.NATIVE_LIB_DIR);
+        env.put("JAVA_HOME", runtimeHome);
+        env.put("HOME", workingDirectory.getAbsolutePath());
+        env.put("TMPDIR", Tools.DIR_CACHE.getAbsolutePath());
+        env.put("LD_LIBRARY_PATH", JREUtils.LD_LIBRARY_PATH);
+        String oldPath = Os.getenv("PATH");
+        env.put("PATH", runtimeHome + "/bin" + (oldPath == null ? "" : ":" + oldPath));
+
+        for (Map.Entry<String, String> entry : env.entrySet()) {
+            if (entry.getValue() != null) {
+                Os.setenv(entry.getKey(), entry.getValue(), true);
+            }
+        }
+        JREUtils.setLdLibraryPath(JREUtils.jvmLibraryPath + ":" + JREUtils.LD_LIBRARY_PATH);
+    }
+}
