478d9905a1ed1d0cae67d3e42acdaf76306a3c53
xonoxo143-ux <xonoxo143@gmail.com>
2026-09-10 00:44:11 -0400

    Pause RT4 audio worker with Android lifecycle

---
+++ singleplayer/client-patches/rt4/AudioThread.java
@@ -0,0 +1,41 @@
+package rt4;
+
+import singleplayer.MobileLifecycleBridge;
+
+/** Mobile-aware RT4 audio worker. */
+public final class AudioThread implements Runnable {
+    public SignLink signLink;
+    public final AudioChannel[] audioChannels = new AudioChannel[2];
+    public volatile boolean shouldStop = false;
+    public volatile boolean isRunning = false;
+    public static volatile Thread workerThread;
+
+    @Override
+    public void run() {
+        workerThread = Thread.currentThread();
+        workerThread.setName("RT4 Audio");
+        isRunning = true;
+        try {
+            while (!shouldStop) {
+                if (MobileLifecycleBridge.isAppPaused()) {
+                    ThreadUtils.sleep(50L);
+                    continue;
+                }
+                for (int i = 0; i < audioChannels.length; i++) {
+                    AudioChannel audioChannel = audioChannels[i];
+                    if (audioChannel != null) {
+                        audioChannel.loop();
+                    }
+                }
+                ThreadUtils.sleep(10L);
+                GameShell.flush(signLink, null);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            TracingException.report(null, ex);
+        } finally {
+            workerThread = null;
+            isRunning = false;
+        }
+    }
+}
